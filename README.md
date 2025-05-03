a3da09f98496d7299fdef6a911f8c3274062f82a
Yoiber dev <yoiber696@gmail.com>
2025-05-02 22:22:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TechDocAPI
+ Sistema API RESTful para gestión de documentación técnica de sistemas instalados en embarcaciones marítimas para Peru Controls System S.A.C.
---
+++ src/main/java/com/perucontrols/techdoc/TechDocApiApplication.java
@@ -0,0 +1,13 @@
+package com.perucontrols.techdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechDocApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechDocApiApplication.class, args);
+	}
+
+}
